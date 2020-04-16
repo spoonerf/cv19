7c14683426cad32b3aa2ba0887e450affcb2a435
Fiona Spooner <spoonerfiona@gmail.com>
2020-04-16 12:41:13 +0100

    just need to change the hospital to run reports

--- Scripts/Running_Hospital_Reports.R
+++ Scripts/Running_Hospital_Reports.R
@@ -4,7 +4,8 @@ rmarkdown::render(
   "Scripts/Staff_Absences.Rmd",
   params = list(hospital = hosp, in_file = paste0("Staff_Absences_",hosp, "_20200408.xlsx")),
   output_file = paste0("/Staff_Absences_",hosp,"_", format(Sys.Date(), "%Y%m%d"), ".pdf"),
-  output_dir = here::here("Outputs"))
+  output_dir = here::here("Outputs")
+  )
   
 hosp <- "NDDH"
 
@@ -12,5 +13,6 @@ rmarkdown::render(
   "Scripts/Staff_Absences.Rmd",
   params = list(hospital = hosp, in_file = paste0("Staff_Absences_",hosp, "_20200408.xlsx")),
   output_file = paste0("/Staff_Absences_",hosp,"_", format(Sys.Date(), "%Y%m%d"), ".pdf"),
-  output_dir = here::here("Outputs"))
+  output_dir = here::here("Outputs")
+  )
 
